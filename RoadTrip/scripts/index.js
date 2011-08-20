0a46e8e0c1a9861d597cb91e814c797ec1ed7a53
ChadMcCallum <chadm@iqmetrix.com>
2011-08-20 10:23:55 -0700

    working direction call

---
+++ RoadTrip/scripts/index.js
@@ -0,0 +1,22 @@
+﻿function getDirections() {
+    var request = {
+        origin: 'Vancouver, BC',
+        destination: 'Regina, SK',
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
+}
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+
+function init() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+
+    getDirections();
+}
