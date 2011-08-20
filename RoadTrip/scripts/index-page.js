68a22875b82778950a643ae20780aa66afec3251
MrHayato <willsonh@iqmetrix.com>
2011-08-20 14:08:42 -0700

    Looks good.

--- RoadTrip/scripts/index-page.js
+++ RoadTrip/scripts/index-page.js
@@ -1,11 +1,43 @@
 ﻿$(document).ready(function () {
-    $('.accordion .head').click(function () {
-        $(this).next().toggle('slow');
+    $('#advanced-show').click(function () {
+        $("#advanced-content").slideToggle(200);
         return false;
-    }).next().hide();
+    });
+
+    $("#checkbox-other").live('change', function (evt) {
+        if ($(evt.currentTarget).attr("checked"))
+            $("#input-other").removeAttr("disabled");
+        else {
+            $("#input-other").attr("disabled", "disabled");
+        }
+    });
+
+    $("#input-origin, #input-destination").focus(function () {
+        if ($(this).val().toLowerCase() === "origin..." || $(this).val().toLowerCase() === "destination...") {
+            $(this).val("");
+        }
+    }).blur(function () {
+        if ($(this).val().trim().toLowerCase() === "") {
+            if ($(this).attr("id") == "input-origin")
+                $(this).val("Origin...");
+            else
+                $(this).val("Destination...");
+        }
+    });
 
     $('#form-submit').click(function () {
-        var url = "mapindex.html?o=" + $("#input-origin").val() + "&d=" + $("#input-destination").val();
+        var food = ($("#checkbox-food").attr("checked") == "checked") ? $("#value-food").val() : 0;
+        var gas = ($("#checkbox-food").attr("checked") == "checked") ? $("#value-gas").val() : 0;
+        var hotel = ($("#checkbox-food").attr("checked") == "checked") ? $("#value-hotel").val() : 0;
+        var other = ($("#checkbox-other").attr("checked") == "checked") ? $("#value-other").val() : "";
+
+        var url = "mapindex.html?" +
+                    "o=" + $("#input-origin").val() +
+                    "&d=" + $("#input-destination").val() +
+                    "&food=" + food +
+                    "&gas=" + gas +
+                    "&hotel=" + hotel +
+                    "&other=" + other;
 
         window.location.href = url;
     });
