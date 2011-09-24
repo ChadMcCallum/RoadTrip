8795ae6444ec945053b2cfcca00befd2b663fd01
MrHayato <willsonh@iqmetrix.com>
2011-09-24 15:14:03 -0700

    Overlay template

--- RoadTrip/scripts/maps.js
+++ RoadTrip/scripts/maps.js
@@ -46,3 +46,64 @@ function getCoordXSecondsIntoTrip(seconds, steps) {
     var distanceTravelled = seconds * kmPerHourToMetersPerSecond(100);
     return getCoordinateXMetersIntoTrip(distanceTravelled, steps);
 }
+
+function PSOverlay(bounds, data, map) {
+    // Now initialize all properties.
+    this.bounds_ = bounds;
+    this.data_ = data;
+    this.map_ = map;
+
+    // We define a property to hold the image's
+    // div. We'll actually create this div
+    // upon receipt of the add() method so we'll
+    // leave it null for now.
+    this.div_ = null;
+
+    // Explicitly call setMap() on this overlay
+    this.setMap(map);
+}
+
+PSOverlay.prototype.onAdd = function() {
+    // Note: an overlay's receipt of onAdd() indicates that
+    // the map's panes are now available for attaching
+    // the overlay to the map via the DOM.
+
+    // Create the DIV and set some basic attributes.
+    var div = $("#overlay-template").tmpl(this.data_);
+
+    // Set the overlay's div_ property to this DIV
+    this.div_ = div;
+
+    // We add an overlay to a map via one of the map's panes.
+    // We'll add this overlay to the overlayImage pane.
+    var panes = this.getPanes();
+    panes.overlayLayer.appendChild(div);
+};
+
+PSOverlay.prototype.draw = function() {
+
+    // Size and position the overlay. We use a southwest and northeast
+    // position of the overlay to peg it to the correct position and size.
+    // We need to retrieve the projection from this overlay to do this.
+    var overlayProjection = this.getProjection();
+
+    // Retrieve the southwest and northeast coordinates of this overlay
+    // in latlngs and convert them to pixels coordinates.
+    // We'll use these coordinates to resize the DIV.
+    var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+    var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+    // Resize the image's DIV to fit the indicated dimensions.
+    var div = this.div_;
+    div.style.left = sw.x + 'px';
+    div.style.top = ne.y + 'px';
+    div.style.width = (ne.x - sw.x) + 'px';
+    div.style.height = (sw.y - ne.y) + 'px';
+};
+
+PSOverlay.prototype.onRemove = function() {
+    this.div_.parentNode.removeChild(this.div_);
+    this.div_ = null;
+};
+
+PSOverlay.prototype = new google.maps.OverlayView();
